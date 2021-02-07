5db41091ce33996dc565e811b14a92dfc18a57b2
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-02-07 19:06:47 +0100

    v1.0

--- bin/queens.py
+++ bin/queens.py
@@ -23,7 +23,7 @@ class queens:
 		"""Add a queen if possible
 		"""
 		if self.queens == line:
-			self.print()
+			self.print() # All queens are placed
 		else:
 			for column in range(self.queens): # Try all possibile position into the same line
 				if self.secure(column, line):
@@ -34,9 +34,9 @@ class queens:
 		"""Check if a cell is secure
 		"""
 		for i in range(line): # Check all old queens
-			if self.queens_pos[i] == column or self.queens_pos[i] == column - (line - i) or self.queens_pos[i] == column + (line - i):
+			if self.queens_pos[i] == column or self.queens_pos[i] == column - (line - i) or self.queens_pos[i] == column + (line - i): # Check the column, and diagonals, we didn't search by line, because it's impossibile have two queens into the same line with this algorithm
 				return False
-		return True
+		return True # If passed all tests it's possible place a new queen in that place
 
 	def print(self):
 		"""Costumized print
@@ -45,21 +45,23 @@ class queens:
 		print(f"Combination #{self.founded}")
 
 		# Intestation
-		board = " " * (len(str(self.queens)) + 2)
+		board = " " * (len(str(self.queens)) + 2) # Initials spaces
 		for i in range(self.queens):
-			board += ascii_uppercase[i % len(ascii_uppercase)] + " "
+			board += ascii_uppercase[i % len(ascii_uppercase)] + " " # Board's letters
 		board += "\n"
 
 		# Body
 		board += " " * (len(str(self.queens)) + 1) + "--" * self.queens + "-\n"
 		for i in range(self.queens):
-			board += str(self.queens - i) + " " * (len(str(self.queens)) - len(str(self.queens - i)) + 1) + "| " * self.queens_pos[i] + "|x" +  "| " * (self.queens - self.queens_pos[i] - 1) + "|" + " " * (len(str(self.queens)) - len(str(self.queens - i)) + 1) + str(self.queens - i) + "\n"
-			board += " " * (len(str(self.queens)) + 1) + "--" * self.queens + "-\n"
+			board += str(self.queens - i) + " " * (len(str(self.queens)) - len(str(self.queens - i)) + 1) # Left number + some spaces
+			board += "| " * self.queens_pos[i] + "|x" +  "| " * (self.queens - self.queens_pos[i] - 1) + "|" # Single line of the board
+			board += " " * (len(str(self.queens)) - len(str(self.queens - i)) + 1) + str(self.queens - i) + "\n" # Right number + some spaces + new line
+			board += " " * (len(str(self.queens)) + 1) + "--" * self.queens + "-\n" # Separator line
 		
 		# Footer
-		board += " " * (len(str(self.queens)) + 2)
+		board += " " * (len(str(self.queens)) + 2)# Initials spaces
 		for i in range(self.queens):
-			board += ascii_uppercase[i % len(ascii_uppercase)] + " "
+			board += ascii_uppercase[i % len(ascii_uppercase)] + " " # Board's letters
 		board += "\n"
 
 		# Print
--- docs/README.md
+++ docs/README.md
@@ -2,7 +2,7 @@
 [![GitHub license](https://img.shields.io/badge/license-GNU-green?style=flat)](https://github.com/CastellaniDavide/cpp-queens/blob/master/LICENSE) ![Author](https://img.shields.io/badge/author-Castellani%20Davide-green?style=flat) ![Version](https://img.shields.io/badge/version-v1.0-blue?style=flat) ![Language Python](https://img.shields.io/badge/language-Python-yellowgreen?style=flat) ![sys.platform supported](https://img.shields.io/badge/OS%20platform%20supported-Linux,%20Windows%20&%20Mac%20OS-blue?style=flat) [![On GitHub](https://img.shields.io/badge/on%20GitHub-True-green?style=flat&logo=github)](https://github.com/CastellaniDavide/queens)
 
 ## Description
-Solution for the 8 queens problem. (support n queens).
+Solution for the 8 queens problem (supports n queens).
 
 ## Required
  - python3
